22196a70838e9180d14ec7b31b4d7007dfe21b8e
samukenji <72771812+samukenji@users.noreply.github.com>
2020-10-28 16:25:51 -0300

    Atribuindo cores às peças
    A partir de agora, comecei a usar o terminal do GIT BASH para execução do programa. Isso permite distinguir as peças em cores diferentes.

--- jogodexadrez/src/aplication/UI.java
+++ jogodexadrez/src/aplication/UI.java
@@ -1,38 +1,60 @@
 //Classe que contém método para imprimir o tabuleiro de xadrez
 package aplication;
 
+import xadrez.Cor;
 import xadrez.Peca_de_xadrez;
 
 public class UI {
-	
+
+	// Será responsável pela cor das peças quando o terminal de saída for o GIT
+	// BASH. (código copiado)
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+
 	// método que imprime tabuleiro
-	public static void printTabuleiro(Peca_de_xadrez[][] tabuleirodexadrez)
-	{
-		for(int i=0; i<tabuleirodexadrez.length; i++)
-		{
-			System.out.print((8-i)+ " ");
-			for(int j=0; j<tabuleirodexadrez.length; j++)
-			{
+	public static void printTabuleiro(Peca_de_xadrez[][] tabuleirodexadrez) {
+		for (int i = 0; i < tabuleirodexadrez.length; i++) {
+			System.out.print((8 - i) + " ");
+			for (int j = 0; j < tabuleirodexadrez.length; j++) {
 				printPeca(tabuleirodexadrez[i][j]);
 			}
 			System.out.println();
 		}
-		System.out.println("  a b c d e f g h");		
+		System.out.println("  a b c d e f g h");
 	}
-	
+
 	// método que imprime uma peça
-	public static void printPeca(Peca_de_xadrez pecadexadrez)
-	{
+	public static void printPeca(Peca_de_xadrez pecadexadrez) {
 		// se não tiver peça em uma posição do tabuleiro, imprimimos um traço
-		if(pecadexadrez==null)
-		{
+
+		if (pecadexadrez == null) {
 			System.out.print("-");
-		}
-		else {
-			System.out.print(pecadexadrez);
+		} else {
+			if (pecadexadrez.getCor() == Cor.BRANCO) {
+				System.out.print(ANSI_WHITE + pecadexadrez + ANSI_RESET);
+			} else {
+				System.out.print(ANSI_YELLOW + pecadexadrez + ANSI_RESET);
+			}
 		}
 		// imprimindo espaço em branco para que as peças não fiquem grudadas
 		System.out.print(" ");
+		
 	}
-	
+
 }
--- jogodexadrez/src/xadrez/Partida_de_xadrez.java
+++ jogodexadrez/src/xadrez/Partida_de_xadrez.java
@@ -9,50 +9,56 @@ public class Partida_de_xadrez {
 
 	// Uma partida de xadrez contém um tabuleiro:
 	private Tabuleiro tabuleiro;
-	
+
 	// O tabuleiro de xadrez contém sempre 8 linhas e 8 colunas
-	public Partida_de_xadrez()
-	{
-		tabuleiro= new Tabuleiro(8, 8);
+	public Partida_de_xadrez() {
+		tabuleiro = new Tabuleiro(8, 8);
 		iniciodejogo();
 	}
-	
+
 	// Método que retorna uma matriz de peças de xadrez para essa essa partida
-	
-	public Peca_de_xadrez[][] getPieces()
-	{
+
+	public Peca_de_xadrez[][] getPieces() {
 		// Criando uma matriz com quantidade de linhas e colunas do tabuleiro.
-		Peca_de_xadrez[][] matriz= new Peca_de_xadrez[tabuleiro.getQddlinhas()][tabuleiro.getQddcolunas()];
-		
-		// Para cada posição dessa matriz eu vou associar uma peça de xadrez. Para isso, preciso usar o downcasting (Peca_de_xadrez)
-		
-		for(int i=0; i<tabuleiro.getQddlinhas(); i++)
-		{
-			for(int j=0; j< tabuleiro.getQddcolunas(); j++)
-			{
-				matriz[i][j]= (Peca_de_xadrez) tabuleiro.peca(i, j);
+		Peca_de_xadrez[][] matriz = new Peca_de_xadrez[tabuleiro.getQddlinhas()][tabuleiro.getQddcolunas()];
+
+		// Para cada posição dessa matriz eu vou associar uma peça de xadrez. Para isso,
+		// preciso usar o downcasting (Peca_de_xadrez)
+
+		for (int i = 0; i < tabuleiro.getQddlinhas(); i++) {
+			for (int j = 0; j < tabuleiro.getQddcolunas(); j++) {
+				matriz[i][j] = (Peca_de_xadrez) tabuleiro.peca(i, j);
 			}
 		}
-		
-		return matriz;	
-	}
-	
-	//Método responsável por iniciar a partida de xadrez, colocando as peças no tabuleiro
-	private void iniciodejogo()
-	{
-		//chamando método iniciar_com_posicao_de_xadrez 
-		iniciar_com_posicao_de_xadrez('b', 6, new Torre(tabuleiro, Cor.BRANCO));
-		iniciar_com_posicao_de_xadrez('e', 8, new Rei(tabuleiro, Cor.BRANCO));
-		iniciar_com_posicao_de_xadrez('e', 1, new Rei(tabuleiro, Cor.BRANCO));
 
+		return matriz;
 	}
-	
-	// Método também responsável por iniciar a partida de xadrez mas informando a posição de xadrez e não a posição da matriz como o método anterior
-	private void iniciar_com_posicao_de_xadrez(char coluna, int linha, Peca_de_xadrez pecadexadrez)
-	{
-		
+
+	// Método responsável por iniciar a partida de xadrez, colocando as peças no
+	// tabuleiro
+	private void iniciodejogo() {
+		// chamando método iniciar_com_posicao_de_xadrez
+		iniciar_com_posicao_de_xadrez('c', 1, new Torre(tabuleiro, Cor.BRANCO));
+		iniciar_com_posicao_de_xadrez('c', 2, new Torre(tabuleiro, Cor.BRANCO));
+		iniciar_com_posicao_de_xadrez('d', 2, new Torre(tabuleiro, Cor.BRANCO));
+		iniciar_com_posicao_de_xadrez('e', 2, new Torre(tabuleiro, Cor.BRANCO));
+		iniciar_com_posicao_de_xadrez('e', 1, new Torre(tabuleiro, Cor.BRANCO));
+		iniciar_com_posicao_de_xadrez('d', 1, new Rei(tabuleiro, Cor.BRANCO));
+
+		iniciar_com_posicao_de_xadrez('c', 7, new Torre(tabuleiro, Cor.PRETO));
+		iniciar_com_posicao_de_xadrez('c', 8, new Torre(tabuleiro, Cor.PRETO));
+		iniciar_com_posicao_de_xadrez('d', 7, new Torre(tabuleiro, Cor.PRETO));
+		iniciar_com_posicao_de_xadrez('e', 7, new Torre(tabuleiro, Cor.PRETO));
+		iniciar_com_posicao_de_xadrez('e', 8, new Torre(tabuleiro, Cor.PRETO));
+		iniciar_com_posicao_de_xadrez('d', 8, new Rei(tabuleiro, Cor.PRETO));
+	}
+
+	// Método também responsável por iniciar a partida de xadrez mas informando a
+	// posição de xadrez e não a posição da matriz como o método anterior
+	private void iniciar_com_posicao_de_xadrez(char coluna, int linha, Peca_de_xadrez pecadexadrez) {
+
 		Posicao_de_xadrez x = new Posicao_de_xadrez(coluna, linha);
 		tabuleiro.colocarpeca(pecadexadrez, x.conversaodepecas());
 	}
-	
+
 }
